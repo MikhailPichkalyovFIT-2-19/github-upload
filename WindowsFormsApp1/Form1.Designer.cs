dadae95df90cb623e9c6846f0bbb63ae9c006dbf
MikhailPichkalyovFIT-2-19 <adveneksanov@gmail.com>
2022-06-02 22:50:50 +0500

    initializing repository

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.языкToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.языкToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.форумToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.русскийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.английскийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.francaisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(431, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Точность";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(346, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "знаков после запятой";
+            // 
+            // языкToolStripMenuItem
+            // 
+            this.языкToolStripMenuItem.Name = "языкToolStripMenuItem";
+            this.языкToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.языкToolStripMenuItem.Text = "Язык";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.языкToolStripMenuItem1,
+            this.форумToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(126, 52);
+            // 
+            // языкToolStripMenuItem1
+            // 
+            this.языкToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.русскийToolStripMenuItem,
+            this.английскийToolStripMenuItem,
+            this.francaisToolStripMenuItem});
+            this.языкToolStripMenuItem1.Name = "языкToolStripMenuItem1";
+            this.языкToolStripMenuItem1.Size = new System.Drawing.Size(125, 24);
+            this.языкToolStripMenuItem1.Text = "Язык";
+            // 
+            // форумToolStripMenuItem
+            // 
+            this.форумToolStripMenuItem.Name = "форумToolStripMenuItem";
+            this.форумToolStripMenuItem.Size = new System.Drawing.Size(125, 24);
+            this.форумToolStripMenuItem.Text = "Форум";
+            // 
+            // русскийToolStripMenuItem
+            // 
+            this.русскийToolStripMenuItem.Name = "русскийToolStripMenuItem";
+            this.русскийToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.русскийToolStripMenuItem.Text = "Русский";
+            // 
+            // английскийToolStripMenuItem
+            // 
+            this.английскийToolStripMenuItem.Name = "английскийToolStripMenuItem";
+            this.английскийToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.английскийToolStripMenuItem.Text = "English";
+            // 
+            // francaisToolStripMenuItem
+            // 
+            this.francaisToolStripMenuItem.Name = "francaisToolStripMenuItem";
+            this.francaisToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.francaisToolStripMenuItem.Text = "Francais";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem языкToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem языкToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem русскийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem английскийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem francaisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem форумToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl((e.KeyChar)))
+                e.Handled = true;
+            base.OnKeyPress(e);
+        }
+        public int znafza = 0;
+        public string vych(string um,string vy)
+        {   string raz="";
+            int uml = um.Length - 1;
+            int vyl=vy.Length - 1;
+            int m = 0;
+            while(vyl>=0)
+            {
+                int a = m ;
+                a+=Convert.ToInt32(um[uml]) - Convert.ToInt32(vy[vyl]);
+                if (a < 0)
+                {
+                    m = -1;
+                    a += 10;
+                }
+                else m = 0;
+                raz=Convert.ToString(a)+raz;
+                uml--;
+                vyl--;
+            }
+            return raz;
+        }
+        public string sqrt(string s)
+        {
+            string result = "",t="";string exresult="";
+            string doublepart="";
+            int i = 0, j = 0 ;
+            string intpart = s.Substring(0, s.Length - (s.IndexOf('.') + 1));
+            if(s.IndexOf('.')!=-1) doublepart= s.Substring(s.IndexOf('.')+1);
+            while(doublepart.Length<znafza*2)
+            {
+                doublepart += "0";
+            }
+            if (intpart.Length % 2 == 1) { t = Convert.ToString(s[0]); intpart= intpart.Substring(1); }
+            else { t = Convert.ToString(s[0]) + Convert.ToString(s[1]); intpart = intpart.Substring(2); }
+            int k = 0;
+                if (Convert.ToInt32(t) > 0)
+                {
+                    do { k++; } while (k * k < Convert.ToInt32(t));
+                }
+            result = Convert.ToString(k);
+            t = vych(t, Convert.ToString(k * k));
+            while(intpart.Length > 0)
+            { 
+                t+= Convert.ToString(intpart[0]) + Convert.ToString(intpart[1]);
+                string b=result*"2";
+                int l = -1;
+                do
+                {
+                    l++;
+                } while ((b + Convert.ToString(l)) * l<t));
+                result+=Convert.ToString(l);
+                t=vych(t, Convert.ToString((b + Convert.ToString(l)) * l));
+                intpart = intpart.Substring(2);
+            }
+            
+            if (znafza>0) exresult = result + '.';
+            while(j<znafza)
+            {
+                t += Convert.ToString(doublepart[0]) + Convert.ToString(doublepart[1]);
+                string b = result * "2";
+                int l = -1;
+                do
+                {
+                    l++;
+                } while ((b + Convert.ToString(l)) * l < t));
+                result += Convert.ToString(l);
+                exresult += Convert.ToString(l);
+                t = vych(t, Convert.ToString((b + Convert.ToString(l)) * l));
+                doublepart = doublepart.Substring(2);
+                j++;
+            }
+            return exresult;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s=textBox1.Text;
+            if (s[0] == '-')
+            { s = s.Substring(1); label1.Text = "i";}
+            label1.Text = sqrt(s)+ label1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length <= 6)
+                znafza = Convert.ToInt32(textBox2.Text);
+            else { znafza = 1000000;
+                textBox2.Text = "1000000";
+            }
+        }
+    }
+}
